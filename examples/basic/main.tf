fe9479451e64eac54153ef8132895ab2a5aca5cd
gabrielmccoll <gabriel@cloudconfusion.co.uk>
2023-01-26 01:28:15 +0000

    working. Improving examples

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -1,3 +1,5 @@
+# Basic example of how to use the module.
+# In this case all resources are in the single resource group.
 module "destroy" {
   source = "gabrielmccoll/selfdestruct/azurerm"
   resource_group_name = azurerm_resource_group.example.name
